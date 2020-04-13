1c34c58aa24122410bcaeaacd3f7408c0a21dcf0
vas59 <vasily.yuryev.a1@gmail.com>
2020-04-13 11:34:07 +0700

    drive scripts

--- Drive.py
+++ Drive.py
@@ -7,24 +7,32 @@ import PID
 
 import json
 
+from DriveScripts.StopSign import StopSign
+
 class Drive:
     def __init__(self, drive_data: Data.DataControl):
         self.drive_data = drive_data
         self.config = json.load(open("./configs/drive_cfg.json", "r"))
+        self.drive_data.set("std_speed", self.config.std_speed)
+        self.pid = PID.PID(kP=self.config.PID.p, kI=self.config.PID.i, kD=self.config.PID.d)
+        self.stop_script = StopSign(self.config.scripts["stop_sign"], self.drive_data)
 
     def run(self):
         e1_d = self.drive_data.get(Utils.E1)
         e2_d = self.drive_data.get(Utils.E2)
-        signs_d = self.drive_data.get(Utils.SIGNS_LIST)
-        if self.config.scripts["stop_sign"].enable == True:
-            self.stop_script()
         
-        # if self.drive_data.get("objd").data ==    
-        # e1_e.data
+        if self.config.scripts["stop_sign"].enable == True:
+            self.stop_script.run()
+        if self.config.scripts["pedistrain_sign"].enable == True:
+            self.pedistrain_script()
+        
+        pid_r = self.pid.calc(e1_d.data*self.config.e1_K + e2_d.data*self.config.e2_K)
     
-    def stop_script(self):
-        pass
-
-    def 
+    def pedistrain_script(self):
+        signs_d = self.drive_data.get(Utils.SIGNS_LIST)
+        pedistrain_sign = "pedistrain" in signs_d
+        # if pedistrain_sign:
+            
+        # pass 
 
 
---
+++ DriveScripts/StopSign.py
@@ -0,0 +1,24 @@
+import Utils
+import Data
+class StopSign:
+    def __init__(self, config, drive_data: Data.DataControl):
+        self.config = config
+        self.drive_data = drive_data
+        self.reaction_en = True
+    
+    def enb(self):
+        self.reaction_en = True
+    def go(self):
+        self.drive_data.set("speed", self.drive_data.get("std_speed").data)
+
+        @Utils.delay(delay=self.config.attention_delay)
+        self.enb()
+    def run(self):
+        signs_d = self.drive_data.get(Utils.SIGNS_LIST)
+        stop_sign = "stop" in signs_d or "stop_sign" in signs_d
+        if stop_sign:
+            self.reaction_en = False
+            self.drive_data.set("speed", 0)
+
+            @Utils.delay(delay=self.config.stop_time)
+            self.go()
