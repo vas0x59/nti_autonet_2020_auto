59ae433d0ab91d15d0296dc2f562a16427f73596
Iliaaer <31032527+Redykail@users.noreply.github.com>
2020-04-14 12:50:27 +0400

    Update func.py

--- baseline/è«ñ/PC/func.py
+++ baseline/è«ñ/PC/func.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 import time
+
 timeout_detect_stop = 0
-KP = 0.32  #0.22
+KP = 0.32  # 0.22
 KD = 0.17
 last = 0
 SIZE = (400, 300)
@@ -28,7 +29,7 @@ pid = 0
 ESCAPE = 27
 SPASE = 32
 
-i=1
+i = 1
 j = 0
 
 IPadress = "192.168.1.104"
@@ -38,22 +39,6 @@ key = 1
 fn = 1
 speed = 1548
 
-
-KP = 0.32  #0.22
-KD = 0.17
-last = 0
-
-SIZE = (400, 300)
-
-RECT = np.float32([[0, 299],
-                   [399, 299],
-                   [399, 0],
-                   [0, 0]])
-
-TRAP = np.float32([[0, 299],
-                   [399, 299],
-                   [320, 200],
-                   [80, 200]])
 
 def detect_stop(perspective):
     global timeout_detect_stop
@@ -70,12 +55,13 @@ def detect_stop(perspective):
     else:
         return False
 
+
 def binarize(img, d=0):
     hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
     binary_h = cv2.inRange(hls, (0, 0, 30), (255, 255, 255))
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    binary_g = cv2.inRange(gray, 150, 255) #130
+    binary_g = cv2.inRange(gray, 150, 255)  # 130
 
     binary = cv2.bitwise_and(binary_g, binary_h)
 
@@ -118,7 +104,7 @@ def find_left_right(perspective, d=0):
 def centre_mass(perspective, d=0):
     hist = np.sum(perspective, axis=0)
     if d:
-        cv2.imshow("Perspektiv2in",perspective)
+        cv2.imshow("Perspektiv2in", perspective)
 
     mid = hist.shape[0] // 2
     i = 0
@@ -128,10 +114,10 @@ def centre_mass(perspective, d=0):
         centre += hist[i] * (i + 1)
         sum_mass += hist[i]
         i += 1
-    if sum_mass>0:
+    if sum_mass > 0:
         mid_mass_left = centre / sum_mass
     else:
-        mid_mass_left = mid-1
+        mid_mass_left = mid - 1
 
     centre = 0
     sum_mass = 0
@@ -140,10 +126,10 @@ def centre_mass(perspective, d=0):
         centre += hist[i] * (i + 1)
         sum_mass += hist[i]
         i += 1
-    if sum_mass>0:
+    if sum_mass > 0:
         mid_mass_right = centre / sum_mass
     else:
-        mid_mass_right = mid+1
+        mid_mass_right = mid + 1
 
     # print(mid_mass_left)
     # print(mid_mass_right)
@@ -157,38 +143,42 @@ def centre_mass(perspective, d=0):
 
     return mid_mass_left, mid_mass_right
 
-class Vision:
-    def __init__():
-        self.last = 0
 
-    def vision_func(frame):
-        img = cv2.resize(frame, (400, 300))
+class Vision:
+    def __init__(self):
+        self.last = 0
+    def vision_func(self, frame):
+        image = frame.copy()
+        img = cv2.resize(image, (400, 300))
         binary = binarize(img, d=1)
         perspective = trans_perspective(binary, TRAP, RECT, SIZE)
-        Detect_Stop_Line = detect_stop(perspective)
-        left, right = centre_mass(perspective, d=1)
-        
-        err = 0 - ((left + right) // 2 - 200)
+        # Detect_Stop_Line = detect_stop(perspective)
 
-        if abs(right - left) < 100:
-            err = last
-            print("LAST")
 
-        angle = int(87 + KP * err + KD * (err - last))
+    def angele(self, frame):
+        image = frame.copy()
+        angle_pd = PD(kP=KP, kD=KD)
+        left, right = centre_mass(image, d=1)
+        angle = angle_pd(left=left, right=right)
         if angle < 70:
             angle = 70
         elif angle > 106:
             angle = 104
         return angle
-      
+
+
 class PD:
     def __init__(self, kP, kD):
         self.kP = kP
         self.kD = kD
         self.prev_error = 0
         self.res = 0
+        self.err = 0
 
-    def calc(self, err):
-        self.res = self.kP * err + self.kD * (err - self.prev_error)
-        self.prev_error = err
+    def calc(self, left, right):
+        self.err = 0 - ((left + right) // 2 - 200)
+        if abs(right - left) < 100:
+            self.err = self.prev_error
+        self.res = int(87 + self.kP * self.err + self.kD * (self.err - self.prev_error))
+        self.prev_error = self.err
         return self.res
