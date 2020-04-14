4f1458321f5e97a22210eecdfb4ebb260f2572fe
Iliaaer <31032527+Redykail@users.noreply.github.com>
2020-04-14 13:30:17 +0400

    Update test_on_video.py

---
+++ baseline/è«ñ/PC/test_on_video.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+from func import *
+
+cap = cv.VideoCapture("video/Sign4.mkv")
+if cap.isOpened() == False:
+    print("Cannot open input video")
+    exit()
+
+vision = Vision()
+b = 0
+i = 0
+while (cv.waitKey(1) != 27):
+    ret, frame = cap.read()
+    frame_copy = frame.copy()
+    perspective = vision.vision_func(frame=frame_copy)
+    cv.imshow("perspective", perspective)
+    print(vision.angele(frame=perspective))
+    stop = detect_stop(perspective)
+    if stop:
+        print(stop)
+        b = perspective.copy()
+        cv.imshow("STOP"+str(i), perspective)
+        i += 1
+
+cv.destroyAllWindows()
+cap.release()
